e6eb8f35d814de371650333c8c80d3fd21ac6655
OJASWI-17 <ojaswijaswal2004@gmail.com>
2024-01-24 18:29:32 +0530

    matcom

---
+++ rectangle.py
@@ -0,0 +1,8 @@
+# wap to print the area and parameter of rectangle
+a=int(input("enter side a :"))
+b=int(input("enter side b :"))
+
+print("area of rectabgle is :",a*b)
+print("parameter of rectangle is :",2*(a+b))
+
+
